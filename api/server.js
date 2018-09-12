ce34aaf4171d61c0ca423a9c57a88005bdb72a9e
varo <varazdat@simplytechnologies.net>
2018-09-12 15:58:13 +0400

    use ssh key and certificate but serve over http

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 import { App } from './app/app';
+import fs from "fs";
 
+const options = {
+    key: fs.readFileSync(`${__dirname}/../server.key`),
+    cert: fs.readFileSync(`${__dirname}/../server.crt`)
+};
 
-
-App.listen(3000);
+App.listen(3000, options);
 
